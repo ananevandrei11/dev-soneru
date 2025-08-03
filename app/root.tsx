02cc0a755b53bd06f654fe74ea2a40ecfa8fdb97
ananevandrei11 <ujlpjcbjlf2011@gmail.com>
2025-08-03 22:19:10 +0200

    layout

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { Header } from "./widgets/Header";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -25,15 +26,17 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
-        {children}
+      <body className="flex flex-col h-full">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <footer>Footer</footer>
         <ScrollRestoration />
         <Scripts />
       </body>
