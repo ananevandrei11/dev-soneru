932da163f29ac9a9a60d1cda6549425b53a7ce42
ananevandrei11 <ujlpjcbjlf2011@gmail.com>
2025-12-07 19:54:59 +0100

    test cloudflare workers 3

--- app/root.tsx
+++ app/root.tsx
@@ -13,7 +13,6 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 import { Header } from "./widgets/Header";
-import type { AppLoadContext } from "react-router";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
